fce19db74b89226b8206f334f8e2954cbb96f3b1
Daniela <daniela.zhao4@gmail.com>
2024-07-19 14:36:53 +0200

    restart

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Homepage from "@/pages/Homepage";
-
-export default function Home() {
-  return (
-    <main className="font-gilroy flex min-h-screen flex-col items-center justify-between p-24">
-      <Homepage />
-    </main>
-  );
-}
--- src/pages/Homepage.js
+++
@@ -1,9 +0,0 @@
-export default function Homepage() {
-  return (
-    <div>
-      <p className="font-sans text-hero-title-70">
-        Hola
-      </p>
-    </div>
-  )
-}
