0671a378e2729f85fbc9339a8c04a07612cf63fb
Daniela <daniela.zhao4@gmail.com>
2024-07-22 15:20:32 +0200

    ft: add logo on header animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import HeaderSection from "./HeaderSection";
 import { MCG_LOGO, NAV_SECTIONS, PROJECTS } from "../utils/constants";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useAnimationControls } from "framer-motion";
+import { motion } from "framer-motion";
+import ConfettiExplosion from 'react-confetti-explosion';
 
 const Header = () => {
 
   const location = useLocation();
+  const animationControls = useAnimationControls();
+
+  const [showConfetti, setShowConfetti] = useState(false);
 
   const checkIfIsProjectPath = (currentPath) => {
     return PROJECTS.some(project => currentPath.includes(project));
@@ -21,15 +27,42 @@ const Header = () => {
     }
   }, []);
 
+  const variants = {
+    still: { y: 0 },
+    bouncing: (i) => ({
+      scale: [1, 1.2, 1],
+      transition: {
+        duration: 0.5,
+      }
+    })
+  };
+
+  const onLogoClick = () => {
+    if (!showConfetti) {
+      animationControls.start('bouncing');
+      setShowConfetti(true);
+    }
+  };
+
   useEffect(() => {
     checkHeaderLogo();
   }, [location, checkHeaderLogo]);
 
   return (
     <div className="fixed top-8 left-12 right-12 bg-white h-fit py-5 px-7 rounded-lg shadow-lg flex flex-row justify-between items-center z-40 sm:top-4 sm:left-6 sm:right-6">
-      <div className="font-bold">
+      <motion.div
+        animate={animationControls}
+        variants={variants}
+        transition={{ type: 'spring', bounce: 0.5, duration: 0.5, repeat: 3, repeatType: 'mirror' }}
+        className="font-bold cursor-pointer"
+        onClick={() => onLogoClick()}
+      >
+        {showConfetti && 
+          <div className="pl-12">
+            <ConfettiExplosion onComplete={() => setShowConfetti(false)} zIndex={50} force={0.4} particleCount={30} />
+          </div>}
         {checkHeaderLogo()}
-      </div>
+      </motion.div>
       <div className="flex flex-row">
         {
           NAV_SECTIONS.map((section) => {
